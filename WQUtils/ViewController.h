9a34aab1a56bc8d667137772105be8d2155b1e0f
wei qi <weiqi@hzdracom.com>
2017-02-07 14:58:24 +0800

    Initial Commit

---
+++ WQUtils/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WQUtils
+//
+//  Created by 魏琦 on 17/2/7.
+//  Copyright © 2017年 com.drcacom.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
